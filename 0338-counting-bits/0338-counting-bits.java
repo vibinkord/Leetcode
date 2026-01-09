d3b13177936829ef41af046c78ce5f562fb9e4f3
VIBIN RICHARD D <90409463+vibinkord@users.noreply.github.com>
2026-01-09 10:35:00 +0530

    Time: 2 ms (96.6%), Space: 49 MB (7.97%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] countBits(int n) {
         int[] dp = new int[n + 1];   
-    dp[0] = 0;                   
+       //dp[0] = 0;                   
 
     for (int i = 1; i <= n; i++) {
         dp[i] = dp[i >> 1] + (i & 1);
