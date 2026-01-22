59de13e86ecdb3a415fb566134e0ab29cc939c8b
VIBIN RICHARD D <90409463+vibinkord@users.noreply.github.com>
2026-01-22 09:55:14 +0530

    Time: 1 ms (50.14%), Space: 47.7 MB (25.65%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -3,12 +3,18 @@ class Solution {
         Map<Integer,Integer> mp=new HashMap<>();
         for(int n:nums){
             mp.put(n,mp.getOrDefault(n,0)+1);
+            if(mp.get(n)>1){
+                return n;
+            }
         }
-        int val=0;
-        for(Map.Entry<Integer,Integer>em:mp.entrySet()){
-            if(em.getValue()>1) val= em.getKey();
-        }
-        return val;
+        // int val=0;
+        // for(Map.Entry<Integer,Integer>em:mp.entrySet()){
+        //     if(em.getValue()>1) {
+        //         val= em.getKey();
+        //         break;
+        //     }
+        // }
+        return -1;
 
     }
 }
