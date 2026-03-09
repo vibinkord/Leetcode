ddab87b36bd7cc163da709a4ceb5937b07620575
VIBIN RICHARD D <90409463+vibinkord@users.noreply.github.com>
2026-03-09 09:57:12 +0530

    Time: 1377 ms (31.86%), Space: 0B (100%) - LeetHub

---
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -0,0 +1,22 @@
+SELECT results FROM (
+    SELECT u.name AS results
+    FROM Users u
+    JOIN MovieRating mr 
+        ON u.user_id = mr.user_id
+    GROUP BY u.user_id
+    ORDER BY COUNT(*) DESC, u.name ASC
+    LIMIT 1
+) a
+
+UNION ALL
+
+SELECT results FROM (
+    SELECT m.title AS results
+    FROM Movies m
+    JOIN MovieRating mr
+        ON m.movie_id = mr.movie_id
+    WHERE mr.created_at BETWEEN '2020-02-01' AND '2020-02-29'
+    GROUP BY m.movie_id
+    ORDER BY AVG(mr.rating) DESC, m.title ASC
+    LIMIT 1
+) b;
