c4df5b6b1774e2310479e9bbfd8746d769e366bc
VIBIN RICHARD D <90409463+vibinkord@users.noreply.github.com>
2026-03-11 09:09:42 +0530

    Time: 1 ms (96.23%), Space: 42.3 MB (96.93%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,5 +1,9 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        return n>0&&(n&(n-1))==0;
+        while(n>1){
+            if(n%2!=0) return false;
+            n/=2;
+        }
+        return n==1;
     }
 }
