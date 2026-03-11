b4192aa435ed2ba5e95fd6fa83fb8c0876a6e2c3
VIBIN RICHARD D <90409463+vibinkord@users.noreply.github.com>
2026-03-11 09:11:29 +0530

    Time: 8 ms (91.35%), Space: 46.1 MB (43.12%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if(n<=0) return false;
-        while(n%3 == 0){
+        while(n>1){
+            if(n%3!=0) return false;
             n/=3;
         }
-        return n == 1;
+        return n==1;
     }
 }
