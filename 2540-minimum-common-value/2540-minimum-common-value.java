014638e6d325daf4f2a5b101130ced67e0eaaa39
VIBIN RICHARD D <90409463+vibinkord@users.noreply.github.com>
2026-05-19 11:44:48 +0530

    Time: 24 ms (5.79%), Space: 94.9 MB (5.73%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int val=0;
+        boolean f=false;
+        Set<Integer> set=new LinkedHashSet<>();
+        for(int i=0;i<nums1.length;i++){
+            set.add(nums1[i]);
+        }
+        for(int i=0;i<nums2.length;i++){
+            if(set.contains(nums2[i])){
+                return nums2[i];
+            }
+        }
+
+        return -1;
+    }
+}
