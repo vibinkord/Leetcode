f8576dc98b98698df45ef7d87c1d49e7feb85091
VIBIN RICHARD D <90409463+vibinkord@users.noreply.github.com>
2026-07-15 16:11:18 +0530

    Time: 34 ms (10.32%), Space: 46.8 MB (29.81%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+
+    List<List<Integer>> ans = new ArrayList<>();
+    HashSet<List<Integer>> set = new HashSet<>();
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+
+        List<Integer> path = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+
+        dfs(nums, path, used);
+
+        return ans;
+    }
+
+    private void dfs(int[] nums, List<Integer> path, boolean[] used) {
+
+        if (path.size() == nums.length) {
+
+            List<Integer> temp = new ArrayList<>(path);
+
+            if (set.add(temp)) {
+                ans.add(temp);
+            }
+
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (used[i])
+                continue;
+
+            used[i] = true;
+            path.add(nums[i]);
+
+            dfs(nums, path, used);
+
+            path.remove(path.size() - 1);
+            used[i] = false;
+        }
+    }
+}
